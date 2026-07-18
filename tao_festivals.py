e53a25a9f0ad843e17dd043453790a1a1132d59d
Yi-bot11 <liyidan_11@163.com>
2026-07-18 18:44:06 +0800

    Add TAO_FESTIVALS data structure with festivals

---
+++ tao_festivals.py
@@ -0,0 +1,74 @@
+# tao_festivals.py
+
+TAO_FESTIVALS = {
+    (1, 1): [
+        {
+            "name": "天腊之辰",
+            "category": "五腊",
+            "level": 5
+        }
+    ],
+    (1, 9): [
+        {
+            "name": "玉皇大帝圣诞",
+            "category": "圣诞",
+            "level": 5
+        }
+    ],
+    (1, 15): [
+        {
+            "name": "上元天官圣诞",
+            "category": "三元",
+            "level": 5
+        }
+    ],
+    (2, 15): [
+        {
+            "name": "太上老君圣诞",
+            "category": "圣诞",
+            "level": 5
+        }
+    ],
+    (3, 3): [
+        {
+            "name": "玄天上帝圣诞",
+            "category": "圣诞",
+            "level": 5
+        }
+    ],
+    (4, 14): [
+        {
+            "name": "吕祖圣诞",
+            "category": "圣诞",
+            "level": 4
+        }
+    ],
+    (6, 24): [
+        {
+            "name": "关圣帝君圣诞",
+            "category": "圣诞",
+            "level": 5
+        }
+    ],
+    (7, 15): [
+        {
+            "name": "中元地官圣诞",
+            "category": "三元",
+            "level": 5
+        }
+    ],
+    (9, 9): [
+        {
+            "name": "斗母元君圣诞",
+            "category": "圣诞",
+            "level": 5
+        }
+    ],
+    (10, 15): [
+        {
+            "name": "下元水官圣诞",
+            "category": "三元",
+            "level": 5
+        }
+    ],
+}
